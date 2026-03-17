e78b6a28e21cf1f7c32a44266f94f1b6b359b0dd
jotavitorz <joaovitorxz812@gmail.com>
2026-03-16 23:19:48 -0300

    security: regenerar chaves do firebase e mover credenciais para variaveis do ambiente

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,14 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyB-8eBu78rG823t-ryxFdpQWZ06vC8t5Ag",
-  authDomain: "linktree-55ddf.firebaseapp.com",
-  projectId: "linktree-55ddf",
-  storageBucket: "linktree-55ddf.firebasestorage.app",
-  messagingSenderId: "236977521092",
-  appId: "1:236977521092:web:b26f49a72ea019f2ac6491"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 
